e4c18f0d47ae6a313857c2624401b178a339c623
JuanziShi <31245638+JuanziShi@users.noreply.github.com>
2020-06-25 09:46:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,20 +9,64 @@ import collection_of_functions as cf
 import numpy as np
 import matplotlib.pyplot as plt
 
-# multiple dipole orientation
-# dip_ori = np.array([[1, 0, 0],[0, 1, 0]]).T
-
 # singele dipole orientation
 dip_ori = np.array([0, 1, 0])
 
+# multiple dipole orientation
+# dip_ori = np.array([[1, 0, 0],[0, 1, 0]]).T
+
 # excitation electric field
-E_ex = cf.get_ex_electric_field()
+E_excitation = cf.get_ex_electric_field()
 
 # Polarizer
-E_polarizer = cf.get_em_polarizer
+E_polarizer = cf.get_em_polarizer()
 
 # excite the sample using linearly polarized light at different angles
-absorption = np.dot(E_ex, dip_ori)
-#plt.plot(np.linspace(0,np.pi,181),absorption)
+E_absorption = np.dot(E_excitation, dip_ori)
+I_absorption = E_absorption ** 2
+# plt.plot(np.linspace(0,np.pi,181),E_absorption)
+# plt.plot(np.linspace(0,np.pi,181),I_absorption)
 
-emission = np.dot(absorption) 
+# Detect the emission polarization by a polarizor
+E_emission = np.dot(E_polarizer, dip_ori)
+I_emission = E_emission ** 2
+# plt.plot(np.linspace(0,np.pi,181),E_emission)
+# plt.plot(np.linspace(0,np.pi,181),I_emission)
+
+# ex * em = 2D polarization image
+I_absorption = np.matrix(I_absorption)
+ 
+
+
+I_absorption = I_absorption.reshape(1,I_absorption.shape[0])
+
+I = np.dot(I_emission, I_absorption)
+
+
+
+
+t1 = np.array([1, 2, 3])
+t2 = t1.reshape(1,t1.shape[0])
+d12 = np.determinant(t1,t2)
+d21 = np.dot(t2,t1)
+
+t1 = np.matrix([1, 2, 3])
+t2 = t1.T
+d21 = np.dot(t2,t1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
